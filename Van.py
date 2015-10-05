dca743dd10e9e53b2c664727a0f2c8941e172025
Jose Araya <joseguillermo.araya@gmail.com>
2015-10-04 23:11:25 -0300

    agrego mas graficos van der Pol

--- Van.py
+++ Van.py
@@ -44,41 +44,55 @@ y0 = 0.1
 
 n_pasos = 1000
 h = 20*np.pi / n_pasos
-y = np.zeros(n_pasos)
-m = np.zeros(n_pasos)
+y_1 = np.zeros(n_pasos)
+m_1 = np.zeros(n_pasos)
 
-y[0] = y0
-m[0] = m0
+y_1[0] = y0
+m_1[0] = m0
 
 for i in range(1,n_pasos):
-    (y[i],m[i]) = avanzar_rk3(y[i-1],m[i-1],h,f)
+    (y_1[i],m_1[i]) = avanzar_rk3(y_1[i-1],m_1[i-1],h,f)
 
 plt.figure(1)
 plt.clf
-plt.plot(y,m,color="r",label="condiciones iniciales: dy/ds=0, y=0.1")
+plt.plot(y_1,m_1,color="r",label="condiciones iniciales: dy/ds=0, y=0.1")
 plt.xlabel('$y$', fontsize=20)
 plt.ylabel("$\\frac{dy}{ds}$",fontsize=20)
+plt.legend(loc='lower right',prop={'size':10})
 plt.title("Oscilador de Van der Pol")
-
+plt.savefig("Van der pol1.png")
 '''condiciones iniciales m0=0 y0=4'''
 m0 = 0
 y0 = 4
 
 n_pasos = 1000
 h = 20*np.pi / n_pasos
-y = np.zeros(n_pasos)
-m = np.zeros(n_pasos)
+y_2 = np.zeros(n_pasos)
+m_2 = np.zeros(n_pasos)
 
-y[0] = y0
-m[0] = m0
+y_2[0] = y0
+m_2[0] = m0
 
 for i in range(1,n_pasos):
-    (y[i],m[i]) = avanzar_rk3(y[i-1],m[i-1],h,f)
+    (y_2[i],m_2[i]) = avanzar_rk3(y_2[i-1],m_2[i-1],h,f)
 
-plt.figure(1)
+plt.figure(2)
 plt.clf
-plt.plot(y,m,color="g",label="condiciones iniciales: dy/ds=0, y=4")
+plt.plot(y_2,m_2,color="g",label="condiciones iniciales: dy/ds=0, y=4")
+plt.xlabel('$y$', fontsize=20)
+plt.ylabel("$\\frac{dy}{ds}$",fontsize=20)
 plt.legend(loc='lower right',prop={'size':10})
+plt.title("Oscilador de Van der Pol")
+plt.savefig("Van der pol2.png")
 
-plt.savefig("Van der pol.png")
+plt.figure(3)
+plt.clf
+plt.plot(y_2,m_2,color="g",label="condiciones iniciales: dy/ds=0, y=4")
+plt.plot(y_1,m_1,color="r",label="condiciones iniciales: dy/ds=0, y=4")
+plt.xlabel('$y$', fontsize=20)
+plt.ylabel("$\\frac{dy}{ds}$",fontsize=20)
+plt.legend(loc='lower right',prop={'size':10})
+plt.title("Oscilador de Van der Pol")
+plt.savefig("Van der pol3.png")
+
 plt.show()
